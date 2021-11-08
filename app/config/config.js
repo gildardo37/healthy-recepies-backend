1957c188b1ecc81c1fa649f54083b7a2ca4223ef
Gildardo <gildardodiaz6937@gmail.com>
2021-11-07 22:35:29 -0800

    Connection to cloud database

--- app/config/config.js
+++ app/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "recipes",
+    HOST: "us-cdbr-east-04.cleardb.com",
+    USER: "b467ec19290523",
+    PASSWORD: "e17f5ad7",
+    DB: "heroku_1456688245aa76d",
     DIALECT: "mysql",
     pool: {
         max: 5,
