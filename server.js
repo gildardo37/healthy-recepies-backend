47578d5e90daa9b1cccf70ff70c6b4ecf821ad26
Gildardo <gildardodiaz6937@gmail.com>
2021-11-26 20:11:19 -0800

    Fix in CORS

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require("./app/config/db.connection");
-require("./app/models/associations");
-
-
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -14,17 +10,19 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(express.json());
 
-require('./app/routes/routes')(app);
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to Healthy Recepies."});
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
 
+require("./app/models/associations");
+require('./app/routes/routes')(app);
 db.connection.sync();
